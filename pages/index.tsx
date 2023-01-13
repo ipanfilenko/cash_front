3bf13619983ed3be336b0059b619d460ea946ae8
yaroslav.sklema <yaroslav.sklema@gmail.com>
2023-01-13 12:13:49 +0200

    add google adsense

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,18 @@ import Search from "../components/Search";
 import Shortcuts from "../components/Shortcuts";
 import News from "../components/News";
 
-
 export default function Home() {
+  const NEXT_PUBLIC_ADSENSE_KEY = process.env.NEXT_PUBLIC_ADSENSE_KEY;
   return (
     <>
       <Head>
         <title>Cash</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${NEXT_PUBLIC_ADSENSE_KEY}`}
+          crossOrigin="anonymous"
+        ></script>
       </Head>
       <Search />
       <Shortcuts />
